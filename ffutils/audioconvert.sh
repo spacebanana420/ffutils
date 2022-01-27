d52efdaab13b7e731fe19774db95a0765efd5a5a
Space Banana <98528301+spacebanana420@users.noreply.github.com>
2022-01-27 22:13:15 +0000

    Flac and vorbis support and major fix
    Support for flac and vorbis has been added
    Replaced "-c:v" with "-c:a" as it was supposed to be an audio encoding specification

--- ffutils/audioconvert.sh
+++ ffutils/audioconvert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Input file"
 read inputfile
-echo -e "Choose an encoder:\n""PCM (wav)\nmp3 (mpegaudio)\naac\nopus (ogg)\nvorbis (ogg)"
+echo -e "Choose an encoder:\n""PCM (wav)\nmp3 (mpegaudio)\naac\nopus (ogg)\nvorbis (ogg)\nflac"
 read encoder
 case ${encoder,,} in
 pcm)
@@ -17,13 +17,19 @@ pcm)
 mp3 || mpegaudio || mpeg audio)
     echo "Bitrate?"
     read $audiorate
-    ffmpeg -i "$inpufile" -c:v libmp3lame -b:a $audiorate audioresult.mp3
+    ffmpeg -i "$inpufile" -c:a libmp3lame -b:a $audiorate audioresult.mp3
 ;;
 opus || ogg opus)
     echo "Bitrate?"
     read $audiorate
-    ffmpeg -i "$inputfile" -c:v libopus -b:a $audiorate audioresult.ogg
+    ffmpeg -i "$inputfile" -c:a libopus -b:a $audiorate audioresult.ogg
 ;;
 vorbis || ogg vorbis)
+    echo "Bitrate?"
+    read $audiorate
+    ffmpeg -i "$inputfile" -c:a vorbis -b:a $audiorate audioresult.ogg
+;;
+flac)
+    ffmpeg -i "$inputfile" -c:a flac audioresult.ogg
 ;;
 esac
