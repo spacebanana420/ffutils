8b0708892301b5a274803ca2e8af8ade0de5ed2d
Space Banana <98528301+spacebanana420@users.noreply.github.com>
2022-01-27 22:14:07 +0000

    Added support for multiple audio coding formats

--- ffutils/x264delivery.sh
+++ ffutils/x264delivery.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
+
+audioparams () {
+case $audioencoder in
+    copy || original || copy original)
+        audioencoding="-c:a copy"
+    ;;
+    opus)
+        echo "Audio bitrate?"
+        read audiobitrate
+        audioencoding="-c:a libopus -b:a $audiobitrate"
+    ;;
+    aac)
+        echo "Audio bitrate?"
+        read audiobitrate
+        audioencoding="-c:a aac -b:a $audiobitrate"
+    ;;
+    flac)
+        audioencoding="-c:a flac"
+    ;;
+    pcm || wav)
+        echo "Audio bit depth?"
+        read audiodepth
+        audioencoding="-c:a pcm_s${audiodepth}le"
+    ;;
+    mp3)
+        echo "Audio bitrate?"
+        read audiobitrate
+        audioencoding="-c:a libmp3lame -b:a $audiobitrate"
+    ;;
+esac
+}
+
 echo "Input file"
 read inputfile
 echo "Choose a preset (from ultrafast to placebo)"
 read preset
-echo "Audio copy? (y/n)"
-read copy
+echo "Select the audio encoding (copy original, opus, aac, flac, pcm, mp3)"
+read audioencoder
 echo "CRF?"
 read crf
-if [ ${copy,,} == y ] || [ ${copy,,} == yes ]
-    then
-    ffmpeg -i "$inputfile" -c:v libx264 -preset:v $preset -crf $crf -c:a copy -pix_fmt yuv420p -bf 16 deliverytrans.mov
-    else
-    echo "Audio bitrate?"
-    read audiobitrate
-    ffmpeg -i "$inputfile" -c:v libx264 -preset:v $preset -crf $crf -c:a libopus -b:a $audiobitrate -pix_fmt yuv420p -bf 16 deliverytrans.mov
-fi
+ffmpeg -i "$inputfile" -c:v libx264 -preset:v $preset -crf $crf $audioencoding -pix_fmt yuv420p -bf 16 delivery_x264.mov
