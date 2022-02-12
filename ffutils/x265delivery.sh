6fbf0bdc49c9f632001761b616fe2d53bf3eb828
Space Banana <98528301+spacebanana420@users.noreply.github.com>
2022-02-12 17:06:14 +0000

    Updated based on latest x264delivery changes

--- ffutils/x265delivery.sh
+++ ffutils/x265delivery.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-audioparams () {
+echo "Input file"
+read inputfile
+echo "Choose a preset (from ultrafast to placebo)"
+read preset
+echo "Select the audio encoding (copy original, opus, aac, flac, pcm, mp3)"
+read audioencoder
 case $audioencoder in
     copy || original || copy original)
         audioencoding="-c:a copy"
@@ -29,21 +34,41 @@ case $audioencoder in
         audioencoding="-c:a libmp3lame -b:a $audiobitrate"
     ;;
 esac
-}
-
-echo "Input file"
-read inputfile
-echo "Choose a preset (from ultrafast to placebo)"
-read preset
-echo "Select the audio encoding (copy original, opus, aac, flac, pcm, mp3)"
-read audioencoder
 echo "CRF?"
 read crf
+echo "Scale video? (y/n)"
+read scaleanswer
+
+if [ ${scaleanswer,,} == "yes" ] || [ ${scaleanswer,,} == "y" ]
+then
+    echo "Scale factor? (example: 0.5; 1.25; 2)"
+    read scalefactor
+    if [ $scalefactor == "0" ] || [ $scalefactor == "" ] || [ $scalefactor == "1" ]
+    then
+        scaleresult=""
+        return
+    fi
+    interpolation="a"
+    while [[ $interpolation != "bilinear" ]] && [[ $interpolation != "bicubic" ]] && [[ $interpolation != "nearest" ]] && [[ $interpolation != "lanczos" ]] && [[ $interpolation != "bicublin" ]] && [[ $interpolation != "" ]]
+    do
+        echo "Choose interpolation (bilinear, bicubic, nearest, lanczos, bicublin) (default=bicubic)"
+        read interpolation
+    done
+    if [ $interpolation != "bilinear" ] || [ $interpolation != "bicubic" ] || [ $interpolation != "nearest" ] || [ $interpolation != "lanczos" ] || [ $interpolation != "bicublin" ]
+    then
+        scaleresult="-vf scale=iw*${scalefactor}:ih*${scalefactor}"
+
+    else
+        scaleresult="-vf scale=iw*${scalefactor}:ih*${scalefactor}:flags=$interpolation"
+    fi
+else
+    scaleresult=""
+fi
 echo "Subsample colour to 4:2:0? (y/n)?"
 read subsample
 if [ ${subsample,,} == y ] || [ ${subsample,,} == yes ]
 then
-    ffmpeg -i "$inputfile" -c:v libx265 -preset:v $preset -crf $crf $audioencoding -pix_fmt yuv420p -bf 16 delivery_x265.mov
+    ffmpeg -i "$inputfile" -c:v libx265 -preset:v $preset -crf $crf $audioencoding $scaleresult -pix_fmt yuv420p delivery_x265.mov
 else
-    ffmpeg -i "$inputfile" -c:v libx265 -preset:v $preset -crf $crf $audioencoding -bf 16 delivery_x265.mov
+    ffmpeg -i "$inputfile" -c:v libx265 -preset:v $preset -crf $crf $audioencoding $scaleresult delivery_x265.mov
 fi
